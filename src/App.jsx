7c7f9ae569538ca43237c702e3f6d0e998611644
Adeline de Gaulejac <adelinedegaulejac@gmail.com>
2025-06-16 16:05:14 +0200

    created chatbot header and messages, imported google fonts icons in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import ChatbotIcon from "./components/ChatbotIcon"
 
 const App = () => {
   return (
-    <div className="containe">
+    <div className="container">
       <div className="chatbot-popup">
-        <div className="chatbot-header">
+        {/* Chatbot Header */}
+        <div className="chat-header">
           <div className="header-info">
             <ChatbotIcon />
             <h2 className="logo-text">Chatbot</h2>
+          </div>
+          <button className="material-symbols-rounded">keyboard_arrow_down</button>
+        </div>
+
+        {/* Chatbot Body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text">Hey there 👋🏽 <br />How can I help you today?</p>
+          </div>
+          <div className="message user-message">
+            <p className="message-text">Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
           </div>
         </div>
       </div>
